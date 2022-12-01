d7b42566ce098deeca9dad727f299c6ecc489b9e
Développeur <developpeur@iMac-de-Alexis-2.local>
2022-12-01 16:55:37 +0100

    majUSER

--- backend/app.js
+++ backend/app.js
@@ -14,14 +14,17 @@ mongoose.connect(process.env.MONGO_URL,
 
 app.use(express.json());
 
-app.use('/api/stuff', stuffRoutes);
-app.use('/api/auth', userRoutes);
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
     next();
 });
+
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+
 
 module.exports = app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,20 +1,21 @@
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User')
 
 exports.signup = (req, res, next) => {
-    res.status(201).json({ message: 'Utilisateur créé !' })
-    // bcrypt.hash(req.body.password, 10)
-    //     .then(hash => {
-    //     const user = new User({
-    //         email: req.body.email,
-    //         password: hash
-    //     });
-    //     user.save()
-    //         .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-    //         .catch(error => res.status(400).json({ error }));
-    //     })
-    //     .catch(error => res.status(500).json({ error }));
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+        const user = new User({
+            email: req.body.email,
+            password: hash
+        });
+        user.save()
+            .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+            console.log(error)
+            .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 exports.login = (req,res,next)=>{
@@ -29,9 +30,13 @@ exports.login = (req,res,next)=>{
                         res.status(401).json({message :'Paire indentifiant/mot de passe incorrecte'})
                     }else{
                         res.status(200).json({
-                            userId : user._id,
-                            token: 'TOKEN'
-                        })
+                            userId: user._id,
+                            token: jwt.sign(
+                                { userId: user._id },
+                                'RANDOM_TOKEN_SECRET',
+                                { expiresIn: '24h' }
+                            )
+                        });
                     }
                 })
                 .catch( error =>{
