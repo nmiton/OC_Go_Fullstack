094018b1e83e9c1af7f9ad45ec4e37283dd89ec5
Nathan <nathan.miton@orange.fr>
2022-12-06 16:22:13 +0100

    maj

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();  
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express()
 
 const stuffRoutes = require('./routes/stuff');
@@ -24,6 +25,7 @@ app.use((req, res, next) => {
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
 
